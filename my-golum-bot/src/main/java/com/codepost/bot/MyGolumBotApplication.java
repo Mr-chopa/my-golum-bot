0c16b63acb071c7636c9ef7887de318f482773ee
GGBZ18 <ggbz18@hcdd.kr>
2017-12-12 14:08:58 +0900

    api test

--- my-golum-bot/src/main/java/com/codepost/bot/MyGolumBotApplication.java
+++ my-golum-bot/src/main/java/com/codepost/bot/MyGolumBotApplication.java
@@ -2,9 +2,16 @@ package com.codepost.bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MyGolumBotApplication {
+public class MyGolumBotApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MyGolumBotApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyGolumBotApplication.class, args);
---
+++ my-golum-bot/src/main/java/com/codepost/bot/controller/BasicController.java
@@ -0,0 +1,12 @@
+package com.codepost.bot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BasicController {
+	@RequestMapping("/**")
+	public String test() {
+		return "Hello, World";
+	}
+}
