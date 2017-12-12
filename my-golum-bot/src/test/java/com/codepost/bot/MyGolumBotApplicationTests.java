648c8e927d2f08d17f03314e5620ab3cc4bea379
GGBZ18 <ggbz18@hcdd.kr>
2017-12-12 11:06:31 +0900

    first commit

---
+++ my-golum-bot/src/test/java/com/codepost/bot/MyGolumBotApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codepost.bot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyGolumBotApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
