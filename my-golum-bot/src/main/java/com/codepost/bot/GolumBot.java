bb503b5af5c1f006fdf62b29cdd4f2ef628d1c4b
GGBZ18 <ggbz18@hcdd.kr>
2017-12-13 14:30:34 +0900

    fix

--- my-golum-bot/src/main/java/com/codepost/bot/GolumBot.java
+++ my-golum-bot/src/main/java/com/codepost/bot/GolumBot.java
@@ -39,7 +39,7 @@ public class GolumBot extends TelegramLongPollingBot {
             sendPhotoRequest.setPhoto("https://www.google.co.kr/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwj3gJr9n4bYAhWENJQKHW2zBGEQjRwIBw&url=https%3A%2F%2Fko.wikipedia.org%2Fwiki%2F%25ED%2595%2598%25ED%258A%25B8_(%25EA%25B8%25B0%25ED%2598%25B8)&psig=AOvVaw1lR-XvIA0isROY4rDiB8-I&ust=1513229138712009");
             try {
             		sendPhoto(sendPhotoRequest);
-                //execute(message); // Call method to send the message
+                execute(message); // Call method to send the message
             } catch (TelegramApiException e) {
                 e.printStackTrace();
             }
